bf3f22cc81ba460cd0be09b660b05c375d1bef75
aditimodi <aditimodi91@gmail.com>
2021-01-06 20:33:56 +0530

    pip

--- pip_pkg_install.sh
+++ pip_pkg_install.sh
@@ -1,19 +1,15 @@
 
 ## install packages using conda
-conda update conda
 
 function install {
   command -v $1 &> /dev/null
 
   if [ $? -ne 0 ]; then             # $? is a special variable contains the exit status of previous command
     echo "Installing: ${1}..."
-    conda install -c conda-forge $1
+    pip install $1
   else
     echo "Already installed: ${1}"
   fi
 }
 
-install tmux
-install jupyter_contrib_nbextensions
-cartopy
-
+palettable
